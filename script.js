f9737a2c308cfcf698a2b8d6746bb43bcf36928b
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-02-03 22:09:57 +0700

    Backup molikom

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#humberger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+const humberger = document.querySelector('#humberger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
